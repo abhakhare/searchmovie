5b6fc7b908e844a3923897371a33be2f815b95a8
Abha <abha.khare06@gmail.com>
2020-07-27 00:30:26 +0530

    code refactored

--- app/src/main/java/com/movie/abha/Constants/Constant.java
+++ app/src/main/java/com/movie/abha/Constants/Constant.java
@@ -1,48 +1,5 @@
 package com.movie.abha.Constants;
 
-/**
- * Created by PremSai on 4/1/2017.
- */
-
 public class Constant {
-
-    public static final String BASE_URL = "";
-    public static final String COUNTRY_CODE = "";
-
-    public static String access_token="access_token";
-    public static String is_login="is_login";
-    public static String login="login";
-    public static String EMAIL="EMAIL";
-    public static String FULL_NAME="FULL_NAME";
-    public static String PASSWORD="PASSWORD";
-    public static String MOBILE="MOBILE";
-    public static String OTP_TOKEN="OTP_TOKEN";
-    public static String USER_ID="USER_ID";
-    public static String DEVICE_TOKEN="DEVICE_TOKEN";
-    public static String CURRENT_BOOKING_ID="CURRENT_BOOKING_ID";
-    public static final double ADDRESS_LOCATOIN_PER_KM = .1;
-
-    public static String LAST_BOOKING_ID="LAST_BOOKING_ID";
-
-    public static String PLACES_AUTOCOMPLETE_API_KEY = "AIzaSyDhb3bII6A6O0CCogK08U6aWpExoLmf-aQ";
     public static int ADDRESS_SEARCH_DELAY = 1000;
-    public static int ADDRESS_GOOGLE_CAMERA = 1000;
-    public static float CAB_NEAR_DISTANCE = 5F;
-    public static int REQUEST_DRIVER_TIMER = 1000 * 60;
-
-    public static String MINI = "mini";
-    public static String MICRO = "micro";
-    public static String SEDAN = "sedan";
-
-    public static String CRN_SYMBOL = "N$";
-
-    public static String ADDRESS_adddress="ADDRESS_adddress";
-    public static String ADDRESS_lat="ADDRESS_lat";
-    public static String ADDRESS_lng="ADDRESS_lng";
-    public static String ADDRESS_pincode="ADDRESS_pincode";
-    public static String ADDRESS_city="ADDRESS_city";
-    public static String ADDRESS_state="ADDRESS_state";
-    public static String ADDRESS_country="ADDRESS_country";
-
-
 }
